de3432a1a33d58efa67f156ac8429293968c6eaa
Jed <avisucam@gmail.com>
2024-03-19 22:41:00 +0800

    Configure custom tailwind colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,21 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      moderateBlue: "#5457b6",
+      softRed: "#ed6468",
+      lightGrayishBlue: "#c3c4ef",
+      paleRed: "#ffb8bb",
+
+      darkBlue: "#324152",
+      grayishBlue: "#67727e",
+      lightGray: "#eaecf1",
+      veryLightGray: "#f5f6fa",
+      white: "#ffffff",
+    },
   },
   plugins: [],
 };
