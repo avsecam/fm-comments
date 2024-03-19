13e7db34deb67b3ebcceed80eddb583b0b344ec1
Jed <avisucam@gmail.com>
2024-03-19 23:21:17 +0800

    Add comment entry.

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,24 @@ import CommentBlock from "./CommentBlock";
 
 export default function Home() {
   return (
-    <main className="flex justify-center w-screen h-screen">
+    <main className="flex justify-center w-screen mb-5">
       <div className="flex flex-col max-w-3xl max-h-fit">
         {data.comments.map((comment, idx) => {
           return <CommentBlock key={idx} comment={comment} />;
         })}
+
+        <article className="flex rounded-lg p-6 bg-white items-start mt-5 gap-5">
+          <form action={(s) => console.log(s)}>
+            <textarea
+              maxLength={140}
+              className="p-4 h-24 resize-none flex-1 border-lightGrayishBlue border rounded-lg placeholder:text-darkBlue"
+              placeholder="Add a comment..."
+            />
+            <button className="w-24 bg-moderateBlue text-white rounded-md p-2.5">
+              SEND
+            </button>
+          </form>
+        </article>
       </div>
     </main>
   );
